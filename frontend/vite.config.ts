0d36977dd6d546d7664f5bc361e70b4c53c6fca5
suryan-s <suryannasa@gmail.com>
2024-04-06 10:35:42 +0530

    Update frontend build configuration and add CORS middleware in main.py

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,9 +7,5 @@ export default defineConfig({
   build: {
     // generate .vite/manifest.json in outDir
     manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: "/src/main.tsx",
-    },
   },
 });
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/", StaticFiles(directory="./frontend/dist", html=True))
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    """
+    Redirects to the Web interface.
+    Returns:
+    """
+    try:
+        return RedirectResponse(url="/")
+    except Exception as error:
+        raise HTTPException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error"
+        ) from error
