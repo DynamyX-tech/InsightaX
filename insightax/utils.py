564ec6b486520aea37d9be65a103953528460afd
suryan-s <suryannasa@gmail.com>
2024-04-06 17:39:24 +0530

    Fix duplicate rows deletion in CustomTrainModel class and update KNearestNeighbour params in train.py

--- insightax/utils.py
+++ insightax/utils.py
@@ -3,7 +3,7 @@ from .train import InsightaX_KNearestNeighbour
 task_types = ["classification", "regression", "clustering",]
 
 task_subtypes = {
-    "classification": [{"binary":"not-available"}, {"logistic":"not-available"}, {"decision-tree":"not-available"}, {"random-forest":"available"}, {"support-vector-machines":"not-available"}, {"k-nearest-neighbors":"available"}, {"neural-networks":"not-available"},],
+    "classification": [{"binary":"not-available"}, {"logistic":"not-available"}, {"decision-tree":"not-available"}, {"random-forest":"not-available"}, {"support-vector-machines":"not-available"}, {"k-nearest-neighbors":"available"}, {"neural-networks":"not-available"},],
     "regression": [{"linear":"not-available"}, {"logistic":"not-available"}, {"support-vector":"not-available"}, {"decision-tree":"not-available"}, {"random-forest":"not-available"},{"gradient-boosting":"not-available"}, {"neural-networks":"not-available"},],
     "clustering": [{"k-means":"not-available"}, {"neural-networks":"not-available"},],
 }
