af96ce8af51e3f2d96c02f6134305079742ecad2
AkashKannan <kannanakash891@gmail.com>
2024-04-06 11:02:36 +0530

    removed templete folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Button } from "@/components/ui/button";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <div>
-          <Button onClick={() => setCount((count) => count + 1)}>
-            {count === 0 ? "Click me" : `Click count: ${count}`}
-          </Button>
-        </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <main></main>;
 }
 
 export default App;
